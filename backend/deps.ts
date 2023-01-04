79843ed474b0906734525ac5c2ac453780134bd2
Gizmo <cunyu@liuli.lol>
2023-01-04 15:28:59 +0800

    chore: 更新依赖

--- backend/deps.ts
+++ backend/deps.ts
@@ -4,9 +4,9 @@ export {
   jsx,
   h,
   serveStatic,
-} from "https://deno.land/x/sift@0.5.0/mod.ts";
+} from "https://deno.land/x/sift@0.6.0/mod.ts";
 export type {
   Routes,
   ConnInfo,
   PathParams,
-} from "https://deno.land/x/sift@0.5.0/mod.ts";
+} from "https://deno.land/x/sift@0.6.0/mod.ts";
